6c08ff93f91969e4516dc2331676f224185b6826
vivek <vivekanandpv@outlook.com>
2023-07-20 16:36:00 +0530

    react-query draft

--- src/Sample.jsx
+++ src/Sample.jsx
@@ -1,7 +1,54 @@
+import axios from 'axios';
+import { useQuery } from 'react-query';
+
+export const getUsers = () => {
+  return axios.get('http://localhost:5000/users').then((r) => r.data);
+};
+
 const Sample = (props) => {
+  const { isLoading, data, isError } = useQuery('get-users', getUsers);
+
+  if (isLoading) {
+    return (
+      <>
+        <h3>Demo Loading...</h3>
+      </>
+    );
+  }
+
+  if (isError) {
+    return (
+      <>
+        <h3>Demo: Can't reach to our servers. Try again...</h3>
+      </>
+    );
+  }
+
   return (
     <>
-      <h3>Sample</h3>
+      <h3>Demo</h3>
+      <table className='table table-bordered table-striped'>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((u) => {
+            return (
+              <tr key={u.id}>
+                <td>{u.id}</td>
+                <td>{u.firstName}</td>
+                <td>{u.lastName}</td>
+                <td>{u.email}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -5,11 +5,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
+import { QueryClientProvider, QueryClient } from 'react-query';
+
+const client = new QueryClient();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
   <BrowserRouter>
-    <App />
+    <QueryClientProvider client={client}>
+      <App />
+    </QueryClientProvider>
   </BrowserRouter>
   // </React.StrictMode>
 );
